4b4824605830e2ae35db5248a969f435df6ffc8d
Bobrovskii13 <bobrovsky.developer@gmail.com>
2026-04-30 18:09:37 +0300

    Added content testing

---
+++ news/tests/test_content.py
@@ -0,0 +1,89 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from datetime import timedelta
+
+from news.models import Comment, News
+from news.forms import CommentForm
+
+User = get_user_model()
+
+
+class TestHomePage(TestCase):
+    """Тесты для главной страницы с новостями."""
+
+    HOME_URL = reverse('news:home')
+    
+    @classmethod
+    def setUpTestData(cls):
+        """Создаем больше новостей, чем отображается на главной странице."""
+        today = timezone.now().date()
+        all_news = []
+        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
+            news = News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
+            all_news.append(news)
+        News.objects.bulk_create(all_news)
+
+    def test_news_count(self):
+        """Тест отображения правильного количества новостей на главной странице."""
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        news_count = object_list.count()
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        """Тест правильного порядка новостей на главной странице (по дате)."""
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        all_dates = [news.date for news in object_list]
+        sorted_dates = sorted(all_dates, reverse=True)
+        self.assertEqual(all_dates, sorted_dates)
+
+
+class TestDetailPage(TestCase):
+    """Тесты для страницы с подробной информацией о новости."""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Создание тестовой новости и комментариев к ней."""
+        cls.news = News.objects.create(
+            title='Тестовая новость', text='Просто текст.'
+        )
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        now = timezone.now()
+        for index in range(10):
+            comment = Comment.objects.create(
+                news=cls.news, author=cls.author, text=f'Tекст {index}',
+            )
+            comment.created = now + timedelta(days=index)
+            comment.save()
+
+    def test_comments_order(self):
+        """Тест правильного порядка комментариев на странице новости (по дате создания)."""
+        response = self.client.get(self.detail_url)
+        self.assertIn('news', response.context)
+        news = response.context['news']
+        all_comments = news.comment_set.all()
+        all_timestamps = [comment.created for comment in all_comments]
+        sorted_timestamps = sorted(all_timestamps)
+        self.assertEqual(all_timestamps, sorted_timestamps)
+    
+    def test_anonymous_client_has_no_form(self):
+        """Тест отсутствия формы для анонимного пользователя на странице новости."""
+        response = self.client.get(self.detail_url)
+        self.assertNotIn('form', response.context)
+        
+    def test_authorized_client_has_form(self):
+        """Тест наличия формы для авторизованного пользователя на странице новости."""
+        self.client.force_login(self.author)
+        response = self.client.get(self.detail_url)
+        self.assertIn('form', response.context)
+        self.assertIsInstance(response.context['form'], CommentForm) 
